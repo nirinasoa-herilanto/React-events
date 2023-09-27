b736699f087a9f2c53073426b7aaf7d0801e150c
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-09-27 09:56:52 +0300

    update: fetching a specific & deleting event

--- client/src/components/Events/EventDetails.jsx
+++ client/src/components/Events/EventDetails.jsx
@@ -1,35 +1,109 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+import { queryClient } from '../../../utils/react-query/queryClient.js';
+
+import { deleteEvent, fetchEvent } from '../../../utils/events/fetchEvents.js';
 
 import Header from '../Header.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { id: params.id }],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: isPendingDelEvent,
+    isError: isErrorDelEvent,
+    error: deletedError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'], exact: true });
+      navigate('/events');
+    },
+  });
+
+  // console.log(`event ${params.id}`, data);
+
+  const deleteEventHandler = () => {
+    mutate({ id: params.id });
+  };
+
+  let content = data && (
+    <article id="event-details">
+      <header>
+        <h1>{data.title}</h1>
+        <nav>
+          {isPendingDelEvent && <p>deleting event ...</p>}
+
+          {isErrorDelEvent && (
+            <ErrorBlock
+              title="An error occurred"
+              message={
+                deletedError.info?.message ||
+                'Failed to delete event. Please try again later!'
+              }
+            />
+          )}
+
+          {!isPendingDelEvent && !isErrorDelEvent && (
+            <button onClick={deleteEventHandler}>Delete</button>
+          )}
+
+          <Link to="edit">Edit</Link>
+        </nav>
+      </header>
+      <div id="event-details-content">
+        <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+        <div id="event-details-info">
+          <div>
+            <p id="event-details-location">{data.location}</p>
+            <time
+              dateTime={`Todo-DateT$Todo-Time`}
+            >{`${data.date} at ${data.time}`}</time>
+          </div>
+          <p id="event-details-description">{data.description}</p>
+        </div>
+      </div>
+    </article>
+  );
+
+  if (isPending) {
+    content = (
+      <div style={{ textAlign: 'center' }}>
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || 'Failed to fetch event'}
+      />
+    );
+  }
+
   return (
     <>
       <Outlet />
+
       <Header>
         <Link to="/events" className="nav-item">
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+
+      {content}
     </>
   );
 }
