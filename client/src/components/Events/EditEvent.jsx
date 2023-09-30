3f1e27ba5c50976bda215948ec9f5be382160346
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-09-30 19:02:10 +0300

    feat: about optimistic updating

--- client/src/components/Events/EditEvent.jsx
+++ client/src/components/Events/EditEvent.jsx
@@ -7,6 +7,7 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../../utils/react-query/queryClient.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
@@ -19,6 +20,33 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async ({ event }) => {
+      await queryClient.cancelQueries({
+        queryKey: ['events', { id: params.id }],
+      }); // will stop query data
+
+      // rollback, if our update event failed
+      // It helps us to get a previous data
+      const previousEvent = queryClient.getQueryData([
+        'events',
+        { id: params.id },
+      ]);
+
+      // console.log(previousEvent);
+
+      queryClient.setQueryData(['events', { id: params.id }], event);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(
+        ['events', { id: params.id }],
+        context.previousEvent
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', { id: params.id }]);
+    },
   });
 
   function handleSubmit(formData) {
